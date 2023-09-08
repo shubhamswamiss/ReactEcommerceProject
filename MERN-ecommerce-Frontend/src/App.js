323a2f460f2e143d7515dbc246e6bdcddec0c511
Shubham <shubhamswamiss333@gmail.com>
2023-09-08 12:34:54 +0530

    CartPage

--- MERN-ecommerce-Frontend/src/App.js
+++ MERN-ecommerce-Frontend/src/App.js
@@ -3,13 +3,8 @@ import './App.css';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import CartPage from './pages/CartPage';
 
 const router = createBrowserRouter([
   {
@@ -24,6 +19,10 @@ const router = createBrowserRouter([
     path: '/signup',
     element: <SignupPage></SignupPage>,
   },
+  {
+    path: '/cart',
+    element: <CartPage></CartPage>
+  }
 ]);
 
 function App() {
